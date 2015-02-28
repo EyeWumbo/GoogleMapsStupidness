7183c9919e6e959c7ae559febbf7c1b36053414b
= <sekhong@uci.edu>
2015-02-28 12:37:08 -0800

    broke things

---
+++ public/javascripts/destruction.js
@@ -0,0 +1,18 @@
+function initialize() {
+		var mapOptions = {
+			center: { lat: -34.397, lng: 150.644},
+			zoom: 8
+			};
+		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+		return map
+		}
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
+      '&signed_in=true&callback=initialize';
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
